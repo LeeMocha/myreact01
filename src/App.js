c7733090976c30f3e37600073981fef0480c26a5
User -2023NGLAA\Administrator <qwertui1217@gmail.com>
2024-03-11 15:00:24 +0900

    2024-03-11 수업내용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import Body from './components/Body';
+import Body from './components02/Body01';
 import Footer from './components/Footer';
 import MyHeader from './components/Header';
+import Recipe from './components01/Recipe';
 // ** import
 // => 컴포넌트는 MyComp from real_File_path; 
 //    내부 코드에서 MyComp 이름으로 인식
@@ -20,11 +21,21 @@ function App() {
   // Test2) Body 로 전달
   const name = "GreenComputer";
 
+  const recipe = {
+    name : "차돌된장찌개", 
+    ingredients : [{name : "소금", amount : "50ml"},
+    {name : "차돌박이", amount:"600g"},
+    {name : "호박", amount:"100g"},
+    {name : "된장", amount:"50ml"}], 
+    steps : ["1단계", "2단계"]
+  }
+
 
   return (
     <div className="App">
       <MyHeader className="App-header" bestDress={bestDress} />
       <Body name={name} country={'경기도 성남시'}/>
+      <Recipe {...recipe}/>
       <Footer />
     </div>
   );
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -22,7 +22,7 @@
 
 // => 입력폼 초기화, 포커스하기 등에 사용
 
-// ** JSX 컴포넌트 기본규칙
+// 1) JSX 컴포넌트 기본규칙
 // => 컴포넌트명은 대문자로 시작 (1컴포넌트 1화일, 대부분 컴포넌트명 과 화일명 동일)
 // => {JS 표현식} : 기본자료형, 산술식,..객체, 배열 등
 // => 단, 컨텐츠 또는 값(리터럴) 을 표현하는 위치에서
@@ -32,13 +32,30 @@
 // => 최상위 Tag 규칙 (필요시 <div> 또는 <React.Fragment> Tag 로 감싸줌)
 // => 조건부 랜더링 : 삼항식({} 내에서 가능), 조건문(JSX 에서는 사용불가능)
 
-// ** Css, 스타일 적용하기
+// 2) Css, 스타일 적용하기
 // => 인라인 스타일링 : style={{스타일...}}
 //    HTML 의경우 <h1 style="color:black; backgroundColor:Orange">
 // => 스타일파일 분리
-//    Body.css (컴포넌트화일명 과 동일)  ,
-//    import './Body.css' 
-// => css import 는 real_File_path 만 명시함
+//  -> 일반 css 화일
+//    - Body.css (컴포넌트화일명 과 동일)  ,
+//    - import './Body.css' : 컴포넌트 imnport 와 달리 real_File_path 만 명시함
+//  -> Sass (syntactically awesome stylesheets, 사스)
+//    - 자주 사용되는 CSS 전처리기 (pre-processor) 중 하나임.
+//    - 확장된 CSS 문법을 사용하여 CSS 코드를 쉽게 작성 할 수 있도록 해주며
+//    - 코드의 재활용성과 가독성을 높여줌
+//    - 확장자 .scss  .sass 를 지원하고 이 둘은 문법의 차이가 있으며
+//       .scss 문법이 더 일반적임.
+//    - 주요 차이점 : .sass 는 중괄호와 {} 와 세미콜론 ;  을 지원하지 않음
+//    - 여러 파일에서 사용되는 내용은 따로 분리하여 관리 
+//      (utils.scss 라는 파일명 주로 사용됨)
+
+// 3) 클래스명 중복 방지 를 위한 이름 규칙 (Naming Rule)
+// => App.css 참고 하면  컴포넌트명-클래스명 형식으로 작성
+// => BEM Naming : BEM 규칙을 준주하며 용도를 명확하게 포함하여 작성
+//    - BEM (Block , Element, Modifier)
+//    - Block: 가장 큰 단위로 독립적인 개체를 의미
+//    - Element: Block을 구성하는 단위로 Block에 속하며 BEM의 Element는 중첩하지 않는다.
+//    - Modifier: 상태를 뜻하는데, 특정한 행동 또는 동작을 의미.
 
 // ** Props, 컴포넌트에 값 전달하기
 // => Props(Properties)객체 : 부모에서 자식으로 값 전달
